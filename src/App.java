ff543b14b1557ca70cf5259520981605fa4b0f95
gabriella jonsson <gabriella.jonsson@elev.ga.ntig.se>
2023-10-23 08:46:32 +0200

    Uppgift a

--- src/App.java
+++ src/App.java
@@ -1,5 +1,22 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+    int litetTal = 10;
+    int stortTal = 1000;
+
+    if(litetTal<1000)
+    {
+    
+        System.out.println(litetTal + " är mindre än "  + stortTal);
+    }
+    else
+    {
+        System.out.println(litetTal  + "är mindre än" + stortTal);
+    }
+
+    System.out.println ( litetTal=litetTal  stortTal = stortTal);
+
+
     }
 }
